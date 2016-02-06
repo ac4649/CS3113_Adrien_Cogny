4ee1f9d058e63bff75b3e7158512eee4f7ce726c
ac4649 <ac4649@nyu.edu>
2016-02-06 11:12:18 -0500

    Loaded Sprites and display them
    I loaded the sprites at their original locations and am trying to
    figure out how to move them individually using the matrix conversions.

--- Assignment1/Assignment1/main.cpp
+++ Assignment1/Assignment1/main.cpp
@@ -5,6 +5,13 @@
 #include <SDL_opengl.h>
 #include <SDL_image.h>
 
+
+#include "Matrix.h"
+#include "ShaderProgram.h"
+
+
+
+
 #ifdef _WINDOWS
 #define RESOURCE_FOLDER ""
 #else
@@ -12,6 +19,19 @@
 #endif
 
 SDL_Window* displayWindow;
+
+GLuint LoadTexture(const char *image_path) {
+    SDL_Surface *surface = IMG_Load(image_path);
+    GLuint textureID;
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, surface->w, surface->h, 0, GL_RGBA,
+                 GL_UNSIGNED_BYTE, surface->pixels);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    SDL_FreeSurface(surface);
+    return textureID;
+}
 
 int main(int argc, char *argv[])
 {
@@ -23,6 +43,26 @@ int main(int argc, char *argv[])
     glewInit();
 #endif
     
+    
+    
+    //setup
+    glViewport(0, 0, 640, 360);
+    
+    ShaderProgram program(RESOURCE_FOLDER"vertex_textured.glsl",RESOURCE_FOLDER"fragment_textured.glsl");
+    
+    //textures
+    GLuint medalTexture = LoadTexture("flat_medal1.png");
+    
+    GLuint smileyTexture = LoadTexture("happy.gif");
+    
+    Matrix projectionMatrix;
+    Matrix modelMatrix;
+    Matrix viewMatrix;
+    
+    
+    projectionMatrix.setOrthoProjection(-3.55,3.55,-2.0f,2.0f,-1.0f, 1.0f);
+    
+    glUseProgram(program.programID);
     
     
     
@@ -38,7 +78,58 @@ int main(int argc, char *argv[])
                 done = true;
             }
         }
+        
         glClear(GL_COLOR_BUFFER_BIT);
+
+        
+        program.setModelMatrix(modelMatrix);
+        program.setProjectionMatrix(projectionMatrix);
+        program.setViewMatrix(viewMatrix);
+        
+        //generate the medal
+        
+        glBindTexture(GL_TEXTURE_2D, medalTexture);
+        
+        float vertices[] = {-0.2, -0.2, 0.2, -0.2, 0.2, 0.2, -0.2, -0.2, 0.2, 0.2, -0.2, 0.2};
+        
+        glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices);
+        glEnableVertexAttribArray(program.positionAttribute);
+        
+        float texCoord[] = {0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0};
+        
+        glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texCoord);
+        glEnableVertexAttribArray(program.texCoordAttribute);
+        
+        
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        
+        
+        glDisableVertexAttribArray(program.positionAttribute);
+        glDisableVertexAttribArray(program.texCoordAttribute);
+        
+        
+        
+        
+        //generate the happy face texture
+        
+        glBindTexture(GL_TEXTURE_2D, smileyTexture);
+        
+        float smileyVertex[] = {-1, -1, -0.6, -1, -0.6, -0.6, -1, -1, -0.6, -0.6, -1, -0.6};
+        glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, smileyVertex);
+        glEnableVertexAttribArray(program.positionAttribute);
+        
+        float smileyTexCoord[] = {0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0};
+        
+        glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, smileyTexCoord);
+        glEnableVertexAttribArray(program.texCoordAttribute);
+        
+        
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        
+        glDisableVertexAttribArray(program.positionAttribute);
+        glDisableVertexAttribArray(program.texCoordAttribute);
+        
+        
         SDL_GL_SwapWindow(displayWindow);
     }
     
